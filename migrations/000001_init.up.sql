a338554c61e7548c4a68c2231d0660178ed0b3e4
Felipe David <felipedavid.huh@gmail.com>
2025-01-18 12:05:40 -0300

    fixing schema errors

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -6,9 +6,9 @@ CREATE TABLE users (
     cellphone TEXT,
     tax_number TEXT,
     birth_date DATE,
-    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    deleted_at TIMESTAMPZ
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMPTZ
 );
 
 CREATE TABLE plan (
@@ -16,53 +16,44 @@ CREATE TABLE plan (
     name TEXT NOT NULL,
     price INT NOT NULL,
     duration_days INT NOT NULL,
-    created_at TIMESTAMPZ DEFAULT NOW (),
-    updated_at TIMESTAMPZ DEFAULT NOW ()
+    created_at TIMESTAMPTZ DEFAULT NOW (),
+    updated_at TIMESTAMPTZ DEFAULT NOW ()
 );
 
 CREATE TABLE subscription (
     id SERIAL PRIMARY KEY,
     plan_id INT NOT NULL REFERENCES plan (id),
     user_id INT NOT NULL REFERENCES users (id),
-    start_date TIMESTAMPZ NOT NULL,
-    end_date TIMESTAMPZ NOT NULL,
+    start_date TIMESTAMPTZ NOT NULL,
+    end_date TIMESTAMPTZ NOT NULL,
     is_active BOOLEAN DEFAULT TRUE,
-    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE transaction (
+CREATE TABLE connector (
     id SERIAL PRIMARY KEY,
-    external_id TEXT, -- pluggy transaction id
-    user_id INTEGER REFERENCES users (id) NOT NULL,
-    account_id INTEGER REFERENCES account (id) NOT NULL,
-    description TEXT NOT NULL,
-    amount INT NOT NULL,
-    currency_code TEXT NOT NULL,
-    transaction_date TIMESTAMPZ NOT NULL,
-    category_id INTEGER REFERENCES category (id),
-    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    deleted_at TIMESTAMPZ
-);
-
-CREATE TABLE account (
-    id SERIAL PRIMARY KEY,
-    external_id TEXT, -- pluggy account id
-    type ACCOUNT_TYPE NOT NULL,
-    subtype ACCOUNT_SUBTYPE,
+    external_id INTEGER NOT NULL, -- pluggy connector id
     name TEXT NOT NULL,
-    balance INT NOT NULL,
-    currency_code TEXT NOT NULL,
-    number TEXT,
-    owner TEXT,
-    tax_number TEXT,
-    bank_data_id INTEGER REFERENCES bank_data (id),
-    credit_data_id INTEGER REFERENCES credit_data (id),
-    fi_connection_id INTEGER REFERENCES fi_connection (id),
-    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    deleted_at TIMESTAMPZ
+    primary_color TEXT NOT NULL,
+    country TEXT NOT NULL,
+    type TEXT NOT NULL,
+    status TEXT NOT NULL,
+    stage TEXT NOT NULL,
+    is_open_finance BOOL NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE fi_connection (
+    id SERIAL PRIMARY KEY,
+    external_id TEXT NOT NULL, -- pluggy item id
+    connector_id INTEGER REFERENCES connector (id) NOT NULL,
+    status TEXT NOT NULL,
+    execution_status TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMPTZ
 );
 
 CREATE TABLE bank_account_data (
@@ -73,8 +64,8 @@ CREATE TABLE bank_account_data (
     overdraft_contracted_limit INTEGER,
     overdraft_used_limit INTEGER,
     unarranged_overdraft_amount INTEGER,
-    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE credit_account_data (
@@ -88,40 +79,49 @@ CREATE TABLE credit_account_data (
     credit_limit INTEGER NOT NULL,
     is_limit_flexible INTEGER NOT NULL,
     status TEXT NOT NULL,
-    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE fi_connection (
+CREATE TABLE account (
     id SERIAL PRIMARY KEY,
-    external_id TEXT NOT NULL, -- pluggy item id
-    connector_id INTEGER REFERENCES connector (id) NOT NULL,
-    status TEXT NOT NULL,
-    execution_status TEXT NOT NULL,
-    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    deleted_at TIMESTAMPZ
-);
-
-CREATE TABLE connector (
-    id SERIAL PRIMARY KEY,
-    external_id INTEGER NOT NULL, -- pluggy connector id
-    name TEXT NOT NULL,
-    primary_color TEXT NOT NULL,
-    country TEXT NOT NULL,
+    external_id TEXT, -- pluggy account id
     type TEXT NOT NULL,
-    status TEXT NOT NULL,
-    stage TEXT NOT NULL,
-    is_open_finance BOOL NOT NULL,
-    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    subtype TEXT NOT NULL,
+    name TEXT NOT NULL,
+    balance INT NOT NULL,
+    currency_code TEXT NOT NULL,
+    number TEXT,
+    owner TEXT,
+    tax_number TEXT,
+    bank_account_data_id INTEGER REFERENCES bank_account_data (id),
+    credit_account_data_id INTEGER REFERENCES credit_account_data (id),
+    fi_connection_id INTEGER REFERENCES fi_connection (id),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMPTZ
 );
 
 CREATE TABLE category (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     user_id INTEGER REFERENCES users (id),
-    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    deleted_at TIMESTAMPZ
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMPTZ
+);
+
+CREATE TABLE transaction (
+    id SERIAL PRIMARY KEY,
+    external_id TEXT, -- pluggy transaction id
+    user_id INTEGER REFERENCES users (id) NOT NULL,
+    account_id INTEGER REFERENCES account (id) NOT NULL,
+    description TEXT NOT NULL,
+    amount INT NOT NULL,
+    currency_code TEXT NOT NULL,
+    transaction_date TIMESTAMPTZ NOT NULL,
+    category_id INTEGER REFERENCES category (id),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMPTZ
 );
